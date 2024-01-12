312d44af528dc9116304f51e7005e6f29ae5d702
Ilya Shestakov <siavol@gmail.com>
2024-01-12 17:19:53 +0200

    Use scaffold

--- app/src/main/java/com/example/markdairy/MainActivity.kt
+++ app/src/main/java/com/example/markdairy/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.markdairy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.markdairy.ui.theme.MarkDairyTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +23,35 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MarkDairyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+
+                Scaffold(
+                    topBar = {
+                        Text("Top app bar")
+                    },
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = {  }) {
+                            Icon(Icons.Default.Add, contentDescription = "Add")
+                        }
+                    }
                 ) {
-                    Greeting("Android")
+                    innerPadding ->
+                        Column(
+                            modifier = Modifier
+                                .padding(innerPadding),
+                            verticalArrangement = Arrangement.spacedBy(16.dp)
+                        ) {
+                            Text(
+                                modifier = Modifier.padding(8.dp),
+                                text =
+                                """
+                                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
+                
+                                    It also contains some basic inner content, such as this text.
+                
+                                    You have pressed the floating action button  times.
+                                """.trimIndent(),
+                            )
+                        }
                 }
             }
         }
